cbd9983b2954873d0cf072ce1a1ed8abdd18a379
yunggenius <yunggenius4@gmail.com>
2022-07-20 00:57:45 +0100

    fully functional nft galley homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,87 @@
+
+
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import {NFTCard} from "./nftCard"
+// import dotenv
 
-const Home: NextPage = () => {
+
+const Home = () => {
+
+  const [wallet, setWalletAddress] = useState("")
+  const [collection, setCollectionAddress] = useState("")
+  const [NFTs, setNFTs] = useState([])
+  const [fetchforCollection, setFetchForCollection] = useState(false)
+
+  const fetchNFTs = async () => {
+    let nfts;
+    console.log("fetching NFTs");
+    const api_key = process.env.API_KEYS
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
+    var requestOptions = {
+      method: 'GET'
+    };
+
+    if (!collection.length) {
+      const fetchURL = `${baseURL}?owner=${wallet}`;
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    } else {
+      console.log("fetching nfts or collection owned by address")
+      const fetchURL = `${baseURL}?owner${wallet}&contracrAddresses%5B%5D=${collection}`
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    }
+
+    if (nfts) {
+      console.log("nfts:", nfts)
+      setNFTs(nfts.ownedNfts)
+    }
+  }
+
+  const fetchNFTsForCollection = async () => {
+    if (collection.length) {
+      var requestOptions = {
+        method: 'GET'
+      };
+      const api_key = "kkkMDXtLRZoxO72ouuuQbdKBwyq"
+      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+      if (nfts) {
+        console.log("NFTs in collection:", nfts)
+        setNFTs(nfts.nfts)
+      }
+    }
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      //some code
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input onChange={(e) => { setWalletAddress(e.target.value) }} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
+        <input onChange={(e) => { setCollectionAddress(e.target.value) }} value={collection} type={"text"} placeholder="Add the collection address"></input>
+        <label className="text-gray-600 "><input onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
+          onClick={
+            () => {
+              if (fetchforCollection) {
+                fetchNFTsForCollection()
+              } else (fetchNFTs())
+            }
+          }
+        >Let's go! </button>
+      </div>
+      <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+        {
+          NFTs.length && NFTs.map(nft => {
+            return (
+              <NFTCard nft={nft}></NFTCard>
+            )
+          })
+        }
+      </div>
     </div>
+
   )
 }
 
